5554c02f1d9ccb754b4f20a9fbd388919ee85797
Nicholas Kline <ndkline@theagora.us>
2018-06-26 09:00:39 -0400

    save course work

--- src/components/CheckFilter.vue
+++ src/components/CheckFilter.vue
@@ -5,18 +5,18 @@
     </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                checked: false
-            }
-        },
-        props: [ 'title' ],
-        methods: {
-            checkFilter() {
-                this.checked = !this.checked;
-                this.$emit('check-filter', 'genre', this.title, this.checked);
-            }
-        }
+export default {
+  data() {
+    return {
+      checked: false
+    };
+  },
+  props: ["title", "category"],
+  methods: {
+    checkFilter() {
+      this.checked = !this.checked;
+      this.$emit("check-filter", this.category, this.title, this.checked);
     }
+  }
+};
 </script>
--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,3 +1,4 @@
+
 <template>
     <div class="movie">
         <div class="movie-col-left">
@@ -5,14 +6,29 @@
         </div>
         <div class="movie-col-right">
             <div class="movie-title">
-                <h2 class="movie-title">{{ movie.Title }}</h2>
+                <h2>{{ movie.Title }}</h2>
                 <span class="movie-rating">{{ movie.Rated }}</span>
+            </div>
+            <div class="movie-sessions">
+                <div v-for="session in filteredSessions(sessions)" class="session-time-wrapper">
+                    <div class="session-time">{{ formatSessionTime(session.time) }}</div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-  props: ["movie"]
+  props: ["movie", "sessions", "day"],
+  methods: {
+    formatSessionTime(raw) {
+      return this.$moment(raw).format("h:mm A");
+    },
+    filteredSessions(sessions) {
+      return sessions.filter(session => {
+        return this.$moment(session.time).isSame(this.day, "day");
+      });
+    }
+  }
 };
 </script>
